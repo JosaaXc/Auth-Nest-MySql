ba125cec39393aa484e2e58287258bbf9e8a14c3
JosaaXC <josafat719@gmail.com>
2024-07-07 21:30:29 -0600

    Mysql connection with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: envs.mysql.port,
+      username: envs.mysql.user,
+      password: envs.mysql.password,
+      database: envs.mysql.database,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,18 @@ import * as joi from 'joi'
 
 interface EnvConfig {
     PORT : number,
+    MYSQL_DATABASE : string,
+    MYSQL_USER : string,
+    MYSQL_PASSWORD : string,
+    MYSQL_PORT : number,
 }
 
 const envVarsSchema = joi.object({
     PORT: joi.number().required(),
+    MYSQL_DATABASE: joi.string().required(),
+    MYSQL_USER: joi.string().required(),
+    MYSQL_PASSWORD: joi.string().required(),
+    MYSQL_PORT: joi.number().required(),
 }).unknown(true);
 
 const { error, value } = envVarsSchema.validate( process.env ) 
@@ -19,4 +27,10 @@ const envVars: EnvConfig = value;
 
 export const envs = {
     port: envVars.PORT,
+    mysql: {
+        database: envVars.MYSQL_DATABASE,
+        user: envVars.MYSQL_USER,
+        password: envVars.MYSQL_PASSWORD,
+        port: envVars.MYSQL_PORT,
+    },
 }
